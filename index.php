050d1a3d59a10771695fa859d51b32fce6c55100
Gabriel Macario <moreirag1212@gmail.com>
2025-01-20 15:19:01 -0300

    Add css/add responsiveness and adjusting code

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ include_once("templates/header.php");
                 <h2 class="post-title">
                     <a href="<?= $BASE_URL ?>post.php?id=<?= $post['id'] ?>"><?= $post['title'] ?></a>
                 </h2>
-                <p class="post-description"><?= $post['description'] ?>"></p>
+                <p class="post-description"><?= $post['description'] ?></p>
                 <div class="tags-container">
                 <?php foreach ($post['tags'] as $tag): ?>
                     <a href="#"><?=$tag?></a>
